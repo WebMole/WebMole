b32122b3069ac16f5e22d4a3e30c2106fed37bea
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2012-08-17 19:18:17 -0400

    fixed init js conflict (tooltip) ;)

--- js/ump-init.js
+++ js/ump-init.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 
   // Load PrettyPrint
@@ -12,60 +11,15 @@ $(document).ready(function() {
 
   // Handlers for buttons and actions
   attachHandlers();
+
+});
+
+function attachHandlers() {
   
   // Init tooltip for btn with 'btn-tooltip' class
   $(".btn-tooltip").tooltip();
-});
 
-function attachHandlers() {
   // Main compute button
-
-  // compute graph with source's input json
-  $('.action_compute').on('click', function() {
-    try
-    {
-      var json = JSON.parse($('.source_input').val());
-      create_graph(json);
-    }
-    catch(err)
-    {
-      txt="There was an with your json data.\n\n";
-      txt+="Error description: " + err.message + "\n\n";
-      txt+="you can try to validate it with JSONLint.\n\n";
-      alert(txt);
-    }
-  });
-  
-  // Temp action for loading ajax file
-  $('.drop-over').on('click', function() {
-    var json = handle_json_file("data-map-example.json");
-    create_graph(json);
-  });
-
-  // Editor Clear button
-  $('.action_clear').on('click', function() {
-    $('.source_input').html('');
-    $('.source_input').val('');
-  });
-=======
-$(document).ready(function() {
-
-  // Load PrettyPrint
-  prettyPrint();
-
-  // Noty test
-  var my_notification = noty({layout: "topRight", type: "information", text: 'noty is alive and javascript is ebabled ;)', timeout: 3500});
-
-  // Initialize the tab :)
-  $('#tab-map-viewier').tab('show');
-
-  // Handlers for buttons and actions
-  attachHandlers();
-});
-
-function attachHandlers() {
-  // Main compute button
-
   // compute graph with source's input json
   $('.action_compute').on('click', function(event) {
     try
@@ -94,5 +48,4 @@ function attachHandlers() {
     $('.source_input').html('');
     $('.source_input').val('');
   });
->>>>>>> Better error handling for json input
 }
